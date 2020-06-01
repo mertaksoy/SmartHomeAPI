31ca0c32187ca35862eac0bb724a96bfea1dab7d
Mert Aksoy <mert.aksoy@codesquare.at>
2020-06-01 15:10:57 +0200

    Rewriting code with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,21 @@
-const tradfriClient = require("node-tradfri-client");
-const http = require('http');
+import {discoverGateway} from "node-tradfri-client";
+import {ServerResponse, createServer} from "http";
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
 let tradfriGateway;
 
-const server = http.createServer((req, res) => {
+const server = createServer((req: any, res: ServerResponse) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Smart Home API is running');
 });
 
 server.listen(port, hostname, () => {
-    tradfriClient.discoverGateway().then((res) => {
-        console.log(res);
-        tradfriGateway = res;
+    discoverGateway().then((discoveredGateway) => {
+        console.log(discoveredGateway);
+        tradfriGateway = discoveredGateway;
     });
     console.log(`Server running at http://${hostname}:${port}/`);
 });
