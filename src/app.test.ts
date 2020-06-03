1b167cbf4e928eb3c8ecb1db7a0b234aaa705115
Mert Aksoy <mert.aksoy@codesquare.at>
2020-06-03 10:55:44 +0200

    Stubbing tradfri client methods

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,6 +2,7 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import sinon from 'sinon';
 import * as tradfri from 'node-tradfri-client';
+import {DiscoveredGateway, TradfriClient} from "node-tradfri-client";
 
 chai.use(chaiHttp);
 chai.should();
@@ -11,7 +12,22 @@ describe('Smart Home API', () => {
     let sandbox = sinon.createSandbox();
 
     beforeEach(function () {
-        sandbox.stub(tradfri, 'discoverGateway').returns(Promise.resolve(null));
+        sandbox.restore();
+
+        const discoveredGateway = {
+            host: 'my-host',
+            name: 'my-name',
+            addresses: ['192.168.0.6'],
+            version: '1'
+        } as DiscoveredGateway;
+        const token = {identity: 'my-identity', psk: 'my-psk'} as { identity: string; psk: string; };
+
+        sandbox.stub(tradfri, 'discoverGateway').returns(Promise.resolve(discoveredGateway));
+        sandbox.stub(TradfriClient.prototype, 'observeGroupsAndScenes').returns(Promise.resolve());
+        sandbox.stub(TradfriClient.prototype, 'observeDevices').returns(Promise.resolve());
+        sandbox.stub(TradfriClient.prototype, 'authenticate').returns(Promise.resolve(token)).withArgs('');
+        sandbox.stub(TradfriClient.prototype, 'connect').returns(Promise.resolve(true));
+
         server = require('./app');
     });
 
@@ -20,7 +36,7 @@ describe('Smart Home API', () => {
             .get('/')
             .end((err: any, res: any) => {
                 res.should.have.status(200);
-                res.text.should.equals('API is running. Discovered gateway: -');
+                res.text.should.equals('API is running. Discovered gateway: my-name');
                 done();
             });
     });
